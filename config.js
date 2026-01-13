a3962efd616aa7f7f6d5b7ce9f393348684aa7c8
shivamshukla11 <shivamshu461@gmail.com>
2026-01-13 19:38:24 +0530

    resolved

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const API_KEY = "Bearer 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";
+export const API_KEY = "Bearer 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";
 export const API_URL = "https://openrouter.ai/api/v1/chat/completions";
